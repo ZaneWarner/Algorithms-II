359cd453b5057b126b929c6a6aa2fc5ccd543da6
ZaneWarner <zane.warner@gmail.com>
2018-08-04 13:34:45 -0700

    implemented Prim's algo

---
+++ GreedySchedulingAndPrimAlgo/PrimMSTAlgorithm.py
@@ -0,0 +1,53 @@
+#This is first coding assignment for Algorithms II from Stanford Lagunita
+#The task is to implement a Prim's algorithm for computing a minimum spanning tree, and report the sum of its edge lengths
+#Using a heap-based solution for improved performance is optional, and will be done here
+
+import heapq
+
+x = []
+with open("edges.txt", 'r') as file:
+    for line in file:
+        splitline = line.split()
+        if len(splitline) == 2:
+            numNodes = int(splitline[0])
+            numEdges = int(splitline[1])
+        if len(splitline) == 3:
+            v1 = int(splitline[0])-1
+            v2 = int(splitline[1])-1
+            edgeLength = int(splitline[2])
+            x.append([v1, v2, edgeLength])
+
+def makeAdjacencyList(x, numNodes):
+    adjacencyList = [[]]*numNodes
+    for edge in x:
+        v1 = edge[0]
+        v2 = edge[1]
+        edgeLength = edge[2]
+        adjacencyList[v1] = adjacencyList[v1] + [[v2, edgeLength]]
+        adjacencyList[v2] = adjacencyList[v2] + [[v1, edgeLength]]
+    return adjacencyList
+
+adjacencyList = makeAdjacencyList(x, numNodes)
+
+def PrimMST(adjacencyList):
+    ##Init
+    nodesSpanned = {0 : 1}
+    edgeLengthTotal = 0
+    edgesCrossingCut = []
+    for edge in adjacencyList[0]:
+        candidateEdge = [edge[1], edge[0]]
+        heapq.heappush(edgesCrossingCut, candidateEdge)
+    ##Init Done
+    while edgesCrossingCut != []:
+        newEdge = heapq.heappop(edgesCrossingCut)
+        newVert = newEdge[1]
+        if newVert not in nodesSpanned:
+            nodesSpanned[newVert] = 1
+            edgeLengthTotal += newEdge[0]
+            for edge in adjacencyList[newVert]:
+                candidateEdge = [edge[1], edge[0]]
+                heapq.heappush(edgesCrossingCut, candidateEdge)
+    return edgeLengthTotal
+
+edgeLengthSum = PrimMST(adjacencyList)
+print(edgeLengthSum)
